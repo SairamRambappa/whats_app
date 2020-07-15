04947b72a981e749249f5d693693db6d0afdb2c4
SairamRambappa <sairamrambappa@gmail.com>
2020-07-15 19:04:23 -0400

    final

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# whatsapp_list_scripts
-Contains scripts for monitoring target whatsapp status 
+# whatsapp_scripts
+Scripts to monitor target's online status
+
+Prerequisite :
+
+1.Chrome browser.
+2.Python with packages - selenium.
+
+
+Items included :
+
+1.Script to monitor the target online status.
+2.Chrome webdriver.
+
+Important things to remember : 
+1.Make sure the contact is atleast in the top 5 people of your recent contacts ( Mandatory ) .
+2.Save the target name as simple as you can , if possible without spaces ( not mandatory ) 
+
+
+Description:
+
+1.Run the script on any terminal , this will open up a chrome browser , please scan the QR code and enter the target's name 
+
+Command : python monitor_whatsapp.py <interval> <duration>
+
+For example if you wanna monitor jack for 2 hours and check the status every 5 min, 
+          python monitor_whatsapp.py 5 2
+
+2.Please dont close the terminal and the chrome browser that the script opened.
+3.Once the script finishes , we will get a csv file with name,time,status information.	  
+                          
+
+
---
+++ whatsapp_monitor.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.common.exceptions import *
+import time
+import sys
+from datetime import datetime,date
+import csv
+
+
+driver = webdriver.Chrome(r"chromedriver")
+driver.get('https://web.whatsapp.com')
+
+input('Enter anything after scanning QR code')
+time.sleep(5)
+name = input('Enter the name of user or group : ')
+
+
+user = driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
+user.click()
+
+time.sleep(5)
+def check_exists_by_xpath():
+    try:
+        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/header/div[2]/div[2]/span').text == "online"
+    except NoSuchElementException as ex:
+        return 0,"offline"
+    return 1,"online"
+
+
+interval_min = sys.argv[1]
+duration = sys.argv[2]
+#Converting interval_min to seconds for time.sleep()
+sleep_param = int(interval_min) * 60
+#Calculation the counter 
+counter_param = int((int(duration) * 3600 ) / sleep_param)
+
+print(sleep_param,counter_param)
+counter = 1
+
+with open("status_timesheet.csv","w+",newline='') as csvfile:
+	fieldnames = ['name', 'time', 'status']
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	writer.writeheader()
+	while True:
+		now = datetime.now()
+		dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+		print(name,dt_string,check_exists_by_xpath()[1])
+		writer.writerow({'name':name, 'time':dt_string,'status': check_exists_by_xpath()[1]})
+		time.sleep(sleep_param)
+		counter += 1
+		if counter == counter_param:
+			break
+
